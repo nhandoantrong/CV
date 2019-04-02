8c9cfd70b0d5bc7b847077dca56588f57d9bcd2f
nhandoantrong <nhandoantrong17498@gmail.com>
2019-04-02 19:01:42 +0700

    update resume

--- js/myJs/main.js
+++ js/myJs/main.js
@@ -25,4 +25,38 @@ getAge=()=>
     else return today.year-myBirthday.year;
 }
 
-getElement('myAge').innerHTML=getAge();
+
+//skill progress
+if($('.skills').length > 0) {
+
+    var el = new SimpleBar($('#resume')[0]).getScrollElement();
+
+    $(el).on('scroll', function() {
+
+        $('.progress .progress-bar').each(function() {
+            var bottom_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_window = $(window).scrollTop() + $(window).height();
+
+            var progressWidth = $(this).data('progress-value') + '%';
+            if (bottom_window > bottom_object) {
+
+                $(this).css({
+                    width: progressWidth
+                });
+                $(this).find('.progress-value').animate({
+                    countNum: parseInt(progressWidth, 10)
+                }, {    
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function() {
+                        $(this).text(Math.floor(this.countNum) + '%');
+                    },
+                    complete: function() {
+                        $(this).text(this.countNum + '%');
+                    }
+                });
+            }
+        });
+
+    });
+}
